802a1ad4d37b36e29430ba2e80fa671a7733aa70
Julian Bright <brightsparc@gmail.com>
2019-09-06 12:05:47 -0700

    Adding data and training and update to transforms

--- myproject/transfer_learning_minc.py
+++ myproject/transfer_learning_minc.py
@@ -116,7 +116,7 @@ wd = 0.0001
 lr_factor = 0.75
 lr_steps = [10, 20, 30, np.inf]
 
-num_gpus = 1
+num_gpus = len(mx.test_utils.list_gpus())
 num_workers = 8
 ctx = [mx.gpu(i) for i in range(num_gpus)] if num_gpus > 0 else [mx.cpu()]
 batch_size = per_device_batch_size * max(num_gpus, 1)
--- myproject/transforms.py
+++ myproject/transforms.py
@@ -1,3 +1,24 @@
+################################################################################
+# Things to keep in mind:
+#
+# 1. ``epochs = 5`` is just for this tutorial with the tiny dataset. please change it to a larger number in your experiments, for instance 40.
+# 2. ``per_device_batch_size`` is also set to a small number. In your experiments you can try larger number like 64.
+# 3. remember to tune ``num_gpus`` and ``num_workers`` according to your machine.
+# 4. A pre-trained model is already in a pretty good status. So we can start with a small ``lr``.
+#
+# Data Augmentation
+# -----------------
+#
+# In transfer learning, data augmentation can also help.
+# We use the following augmentation in training:
+#
+# 2. Randomly crop the image and resize it to 224x224
+# 3. Randomly flip the image horizontally
+# 4. Randomly jitter color and add noise
+# 5. Transpose the data from height*width*num_channels to num_channels*height*width, and map values from [0, 255] to [0, 1]
+# 6. Normalize with the mean and standard deviation from the ImageNet dataset.
+#
+
 from mxnet.gluon.data.vision import transforms
 
 
